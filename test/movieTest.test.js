4118c7cfe7f93666e707907123ea5c54e049f0a5
Luiz Barbosa <luiz.barbosa@usemobile.xyz>
2021-05-23 11:15:03 -0300

    feat: add Filter scree test

--- test/movieTest.test.js
+++ test/movieTest.test.js
@@ -1,4 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react-native';
+import FilterMovie from '../src/container/filterMovie';
 
-test('Array with 10 itens ', () => {
-    expect(1 + 2).toBe(3);
-  });
+describe("Filter Movie tests", () => {
+  const { getByPlaceholderText, getByText } = render(<FilterMovie/>);
+  const onEventMock = jest.fn();
+
+  test('Verify placeholder itens', () => {
+    fireEvent(getByPlaceholderText('Filtrar filme por nome'));
+    fireEvent(getByPlaceholderText('Filtrar por ano'));
+  })
+
+  test('Verify button exist', () => {
+    fireEvent(getByText("Filtrar"))
+  })
+
+  test('Change name input text for filter', () => {
+    fireEvent(getByPlaceholderText('Filtrar filme por nome'), 'onChangeText', 'word war z');
+    expect(onEventMock).toHaveBeenCalledWith('word war z');
+  })
+
+  test('Change year input text for filter', () => {
+    fireEvent(getByPlaceholderText('Filtrar filme por ano'), 'onChangeText', '2019');
+    expect(onEventMock).toHaveBeenCalledWith('2019');
+  })
+
+  test('Press Button for filter', () => {
+    fireEvent.press(getByText('Press me'));
+    expect(onPressMock).toHaveBeenCalled(); 
+  })
+})
