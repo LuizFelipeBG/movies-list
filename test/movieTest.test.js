c30e9fa2f571c8adc912ee18de275589c58a376c
Luiz Barbosa <luiz.barbosa@usemobile.xyz>
2021-05-23 11:40:05 -0300

    feat: change test verify componet filter

--- test/movieTest.test.js
+++ test/movieTest.test.js
@@ -1,33 +1,20 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { render, fireEvent } from '@testing-library/react-native';
 import FilterMovie from '../src/container/filterMovie';
 
 describe("Filter Movie tests", () => {
-  const { getByPlaceholderText, getByText } = render(<FilterMovie/>);
-  const onEventMock = jest.fn();
 
-  test('Verify placeholder itens', () => {
-    fireEvent(getByPlaceholderText('Filtrar filme por nome'));
-    fireEvent(getByPlaceholderText('Filtrar por ano'));
-  })
+  test('examples of some things', async () => {
+    const { getByTestId, getByText, queryByTestId, toJSON } = render(<FilterMovie />)
+    const famousProgrammerInHistory = 'Ada Lovelace'
+  
+    const inputName = getByTestId('InputFilterName')
+    fireEvent.changeText(inputName, famousProgrammerInHistory)
 
-  test('Verify button exist', () => {
-    fireEvent(getByText("Filtrar"))
-  })
-
-  test('Change name input text for filter', () => {
-    fireEvent(getByPlaceholderText('Filtrar filme por nome'), 'onChangeText', 'word war z');
-    expect(onEventMock).toHaveBeenCalledWith('word war z');
-  })
-
-  test('Change year input text for filter', () => {
-    fireEvent(getByPlaceholderText('Filtrar filme por ano'), 'onChangeText', '2019');
-    expect(onEventMock).toHaveBeenCalledWith('2019');
-  })
-
-  test('Press Button for filter', () => {
-    fireEvent.press(getByText('Press me'));
-    expect(onPressMock).toHaveBeenCalled(); 
+    const inputYear = getByTestId('InputFilterYear')
+    fireEvent.changeText(inputYear, famousProgrammerInHistory)    
+  
+    const button = getByText('filterSubmit')
+    fireEvent.press(button)
   })
 })
