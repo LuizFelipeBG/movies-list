1c58d15ad6d2a76ce3780b92c1ebd62e862ce7ce
Luiz Barbosa <luiz.barbosa@usemobile.xyz>
2021-05-24 02:02:29 -0300

    feat: tried to test

--- test/movieTest.test.js
+++ test/movieTest.test.js
@@ -3,18 +3,14 @@ import { render, fireEvent } from '@testing-library/react-native';
 import FilterMovie from '../src/container/filterMovie';
 
 describe("Filter Movie tests", () => {
+  const { getByTestId } = render(<FilterMovie />)
+  const famousProgrammerInHistory = 'Ada Lovelace'
 
-  test('examples of some things', async () => {
-    const { getByTestId, getByText, queryByTestId, toJSON } = render(<FilterMovie />)
-    const famousProgrammerInHistory = 'Ada Lovelace'
-  
+  test('Verify if exists input', async () => {
     const inputName = getByTestId('InputFilterName')
     fireEvent.changeText(inputName, famousProgrammerInHistory)
-
     const inputYear = getByTestId('InputFilterYear')
     fireEvent.changeText(inputYear, famousProgrammerInHistory)    
-  
-    const button = getByText('filterSubmit')
-    fireEvent.press(button)
   })
+  
 })
